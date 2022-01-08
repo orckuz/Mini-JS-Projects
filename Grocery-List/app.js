d3598978fcf0423f8704c9c45bf611bd455e5397
orckuz <ffferrydev@gmail.com>
2022-01-08 14:26:56 -0500

    Html second pass, start of JS file and pseudo code the problem

--- Grocery-List/app.js
+++ Grocery-List/app.js
@@ -1,1 +1,21 @@
-//
+/*PSEUDO CODE*/
+//1.EventListener on keyup for Input item
+//2.Input createElement li and add items(li) to ul
+//3.EventListener on click add classlist to li (for crossing word)
+//4.EventListener on click pen img, remove all items 
+
+const input = document.querySelector('#input');
+const list = document.querySelector('#items');
+
+function newElement() {
+  let newLi = document.createElement('li');
+  let newItem = input.elements.itemInput.value;  
+  newLi.append(newItem);
+console.log(newItem)
+}
+
+input.addEventListener('keydown', function(event) {
+  if (event.keyCode === 13) {
+    newElement();
+  }
+});
