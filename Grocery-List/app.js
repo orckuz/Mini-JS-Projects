6a553230eb483b0e9ddbcc679fe3c2a512d36c34
orckuz <ffferrydev@gmail.com>
2022-01-08 20:38:56 -0500

    All javascript functionality done. TODO: tweak style.css and polish

--- Grocery-List/app.js
+++ Grocery-List/app.js
@@ -1,18 +1,20 @@
 /*PSEUDO CODE*/
 //1.EventListener on keyup for Input item --DONE
 //2.Input createElement li and add items(li) to ul --DONE
-//3.EventListener on click add classlist to li (for crossing word)
+//3.EventListener on click add textDecoration to li (line-through) --DONE
 //4.EventListener on click img remove all items from list
 
 const input = document.querySelector("input");
-const list = document.querySelector("#items");
+const list = document.querySelector("#list");
+const listItems = document.querySelector("#items");
+const remove = document.querySelector("#img");
 
 function newElement() {
   let newLi = document.createElement("li");
   let newItem = input.value;
   newLi.append(newItem);
-  list.append(newLi);
-  console.log(newItem);
+  listItems.append(newLi);
+  input.value = "";
 }
 
 input.addEventListener("keydown", function (evt) {
@@ -21,8 +23,18 @@ input.addEventListener("keydown", function (evt) {
   }
 });
 
-list.addEventListener("click", function (evt) {
+listItems.addEventListener("click", function (evt) {
   if (evt.target.nodeName === "LI") {
     evt.target.style.textDecoration = "line-through";
   }
 });
+
+remove.addEventListener("click", function (evt) {
+  // while (listItems.length > 0) {
+  //   listItems.remove();
+  // }
+  while (listItems.firstChild) {
+    listItems.removeChild(listItems.firstChild);
+  }
+});
+console.log(listItems.firstChild);
