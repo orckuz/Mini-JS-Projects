d1d5404cfc0399a0ae7df604eb19e5b71fa71d9f
orckuz <ffferrydev@gmail.com>
2022-01-10 22:18:58 -0500

    html DONE, JS DONE, TODO: polish Css, and fix icon buttons

--- Tip-Calculator/app.js
+++ Tip-Calculator/app.js
@@ -7,6 +7,7 @@ const percentage = document.querySelector("#percentage");
 const bill = document.querySelector("#bill");
 const guests = document.querySelector("#guest-num");
 const button = document.querySelector("#btn");
+const alertBox = document.querySelector("#tip-amnt");
 
 function percentCalc() {
   return parseInt(percentage.value) / 100;
@@ -16,6 +17,23 @@ function totalTip() {
   return (bill.value * percentCalc()) / guests.value;
 }
 
+function showTipAmnt() {
+  let x = document.querySelector("#tip-amnt");
+  x.classList = "show";
+  setTimeout(() => {
+    x.classList.replace("show", "");
+  }, 3000);
+}
+
 button.addEventListener("click", function () {
-  alert(totalTip());
+  if (totalTip === NaN) {
+    alertBox.textContent = "Total tip $0";
+    showTipAmnt();
+  } else {
+    alertBox.textContent = `Total tip is: $${totalTip()}`;
+    showTipAmnt();
+  }
+  percentage.value = "";
+  bill.value = "";
+  guests.value = "";
 });
