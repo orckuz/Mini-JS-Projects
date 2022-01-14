71cba5a5816580655e22188cd8c28f7d38959ace
orckuz <ffferrydev@gmail.com>
2022-01-14 14:47:07 -0500

    All important JS functionality DONE

--- Expense-Tracker/app.js
+++ Expense-Tracker/app.js
@@ -9,19 +9,45 @@ const nameIpt = document.querySelector("#name");
 const dateIpt = document.querySelector("#date");
 const amntIpt = document.querySelector("#amount");
 
-addBtn.addEventListener("click", function (e) {
+const sampleTr = document.createElement("tr");
+const sampleItem = document.createElement("td");
+sampleItem.textContent = "No items added!";
+sampleItem.colSpan = 4;
+sampleTr.append(sampleItem);
+tBody.append(sampleTr);
+
+// while (!tBody.childElementCount !== 2) {
+//   tBody.append(sampleTr);
+// }
+
+function newExpenseItem() {
   let item = nameIpt.value;
   let date = dateIpt.value;
   let amount = amntIpt.value;
+  const deleteBtn = document.createElement("button");
   const newTr = document.createElement("tr");
   const itemTd = document.createElement("td");
   const dateTd = document.createElement("td");
   const amountTd = document.createElement("td");
+  const deleteTd = document.createElement("td");
+  deleteBtn.textContent = "X";
+  deleteTd.append(deleteBtn);
+  deleteBtn.addEventListener("click", deleteExpense);
   itemTd.append(item);
   dateTd.append(date);
   amountTd.append(amount);
-  newTr.append(itemTd);
-  newTr.append(dateTd);
-  newTr.append(amountTd);
-  console.log(newTr);
+  newTr.append(itemTd, dateTd, amountTd, deleteTd);
+  return tBody.append(newTr);
+}
+
+function deleteExpense() {
+  this.parentElement.parentElement.remove(this);
+}
+
+addBtn.addEventListener("click", function (e) {
+  newExpenseItem();
+  sampleTr.remove(this);
+  nameIpt.value = "";
+  dateIpt.value = "";
+  amntIpt.value = "";
 });
